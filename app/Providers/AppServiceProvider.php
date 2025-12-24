2550a0f498ef21e3b6ffe85200a7130737279d9e
unknown <l200220102@student.ums.ac.id>
2025-12-24 12:46:49 +0700

    baru

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,32 @@ class AppServiceProvider extends ServiceProvider
     {
         // Alias untuk middleware CheckRole
         Route::aliasMiddleware('role', RoleMiddleware::class);
-     
+
+        // Create storage symlink manually if it doesn't exist
+        // This is a workaround for cPanel where exec() function is disabled
+        $this->createStorageLink();
+    }
+
+    /**
+     * Create storage symlink manually
+     */
+    private function createStorageLink(): void
+    {
+        $link = public_path('storage');
+        $target = storage_path('app/public');
+
+        // Check if symlink already exists
+        if (file_exists($link) || is_link($link)) {
+            return;
+        }
+
+        // Create the symlink using PHP's symlink function
+        if (function_exists('symlink')) {
+            try {
+                symlink($target, $link);
+            } catch (\Exception $e) {
+                // Silently fail, symlink might not be available on all systems
+            }
+        }
     }
 }
